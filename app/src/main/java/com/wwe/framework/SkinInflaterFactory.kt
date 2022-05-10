b8f1b0ac80736c6bb5b7700341ecbec75bd0e5a5
44122196 <18588744489@163.com>
2022-05-10 09:41:35 +0800

    Update

---
+++ app/src/main/java/com/wwe/framework/SkinInflaterFactory.kt
@@ -0,0 +1,19 @@
+package com.wwe.framework
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+
+// LayoutInflaterFactory marked as Deprecated
+class SkinInflaterFactory :  LayoutInflater.Factory2 {
+
+    override fun onCreateView(parent: View?, name: String, context: Context, attrs: AttributeSet): View? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateView(p0: String, p1: Context, p2: AttributeSet): View? {
+        TODO("Not yet implemented")
+    }
+}
