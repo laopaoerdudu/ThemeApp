50211cf2d0fbc7e3d04345c6423c9d4f6c7e2e94
David B ZHANG <18588744489@163.com>
2022-06-19 14:07:55 +0800

    Update

--- app/src/main/java/com/wwe/framework/SkinInflaterFactory.kt
+++ app/src/main/java/com/wwe/framework/SkinInflaterFactory.kt
@@ -13,7 +13,8 @@ class SkinInflaterFactory :  LayoutInflater.Factory2 {
         TODO("Not yet implemented")
     }
 
-    override fun onCreateView(p0: String, p1: Context, p2: AttributeSet): View? {
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        // 如果在你在重写的方法中返回 null 的话，就会以系统默认的方式去创建 View
         TODO("Not yet implemented")
     }
 }
